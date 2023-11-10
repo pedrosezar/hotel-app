63a496335e084701b246b7ce493cf9229dc3ec8f
pedrosezar <pedrosezar@gmail.com>
2023-11-09 23:30:12 -0300

    Implementation of Screens and Layouts

--- app/(auth)/(home)/details.tsx
+++ app/(auth)/(home)/details.tsx
@@ -2,61 +2,77 @@ import {
   View,
   StyleSheet,
   Image,
+  Text,
   SafeAreaView,
   ScrollView,
 } from "react-native";
 import React from "react";
 import { useGlobalSearchParams } from "expo-router";
-import { Text, Button } from "@rneui/base";
+import { Button } from "@rneui/base";
 import Constants from "expo-constants";
+import { data } from "../../../mocks/data";
 
 export default function details() {
-  const { cover, name, price, gallery } = useGlobalSearchParams();
-
+  const { id } = useGlobalSearchParams();
+  const { name, price, gallery } = data[0];
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
-        <Text h2>Detalhes Hotel</Text>
-        <View style={styles.item}>
-          <Image
-            style={styles.cover}
-            source={{
-              uri: "https://images.pexels.com/photos/2041556/pexels-photo-2041556.jpeg",
-            }}
-          />
-
-          <View style={styles.info}>
-            <Text h3>{name}</Text>
-            <Text h4>R$ {price}</Text>
-          </View>
-        </View>
+        <Text style={styles.title}>{name}</Text>
 
         <View style={styles.gallery}>
           <Image
             style={styles.photoGallery}
             source={{
-              uri: "https://images.pexels.com/photos/3659683/pexels-photo-3659683.jpeg",
+              uri: gallery[0].photo,
             }}
           />
 
           <Image
             style={styles.photoGallery}
             source={{
-              uri: "https://images.pexels.com/photos/2952663/pexels-photo-2952663.jpeg",
+              uri: gallery[1].photo,
             }}
           />
           <Image
             style={styles.photoGallery}
             source={{
-              uri: "https://images.pexels.com/photos/258154/pexels-photo-258154.jpeg",
+              uri: gallery[2].photo,
             }}
           />
           <Image
             style={styles.photoGallery}
             source={{
-              uri: "https://images.pexels.com/photos/941861/pexels-photo-941861.jpeg",
+              uri: gallery[3].photo,
             }}
           />
+        </View>
+
+        <View style={styles.item}>
+          <View style={styles.info}>
+            <Text style={styles.infoTitle}>Preço para 1 diária, 2 adultos</Text>
+            <Text style={styles.infoPrice}>R$ {price}</Text>
+            <Text style={styles.infoDetail}>Impostos e taxas incluidos</Text>
+          </View>
+        </View>
+
+        <View style={styles.item}>
+          <View style={styles.detail}>
+            <View style={styles.detailLeft}>
+              <Text style={styles.detailTitle}>Check-in</Text>
+              <Text style={styles.detailDate}>ter 17 out</Text>
+            </View>
+            <View style={styles.detailRight}>
+              <Text style={styles.detailTitle}>Check-out</Text>
+              <Text style={styles.detailDate}>qua 18 out</Text>
+            </View>
+            <View style={styles.detailInfo}>
+              <Text style={styles.detailInfoTitle}>Quartos e hóspedes</Text>
+              <Text style={styles.detailInfoSubTitle}>
+                1 quarto, 2 adultos, 0 crianças
+              </Text>
+            </View>
+          </View>
         </View>
 
         <Button title="Reservar" containerStyle={styles.reservar} />
@@ -72,19 +88,9 @@ const styles = StyleSheet.create({
     padding: 16,
     marginTop: Constants.statusBarHeight,
   },
-  item: {
-    backgroundColor: "#fff",
-    flexDirection: "row",
-    margin: 15,
-  },
-  cover: {
-    width: 80,
-    height: 120,
-  },
-  info: {
-    flexDirection: "column",
-    alignItems: "flex-start",
-    marginLeft: 10,
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
   },
   gallery: {
     backgroundColor: "#eee",
@@ -96,9 +102,67 @@ const styles = StyleSheet.create({
     width: 92,
     height: 60,
   },
+  item: {
+    backgroundColor: "#eee",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    marginTop: 10,
+  },
+  info: {
+    flexDirection: "column",
+    alignItems: "flex-start",
+    marginLeft: 10,
+    paddingTop: 5,
+    paddingBottom: 5,
+  },
+  infoTitle: {
+    fontSize: 10,
+  },
+  infoPrice: {
+    fontWeight: "bold",
+    fontSize: 12,
+  },
+  infoDetail: {
+    fontSize: 9,
+  },
+  detail: {
+    width: "100%",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    marginLeft: 10,
+    paddingTop: 5,
+    paddingBottom: 5,
+    gap: 5,
+  },
+  detailLeft: {
+    width: "25%",
+    flexDirection: "column",
+  },
+  detailRight: {
+    flex: 1,
+  },
+  detailTitle: {
+    fontWeight: "bold",
+    fontSize: 11,
+  },
+  detailDate: {
+    color: "#0767F7",
+    fontSize: 10,
+  },
+  detailInfo: {
+    width: "100%",
+  },
+  detailInfoTitle: {
+    fontWeight: "bold",
+    fontSize: 11,
+  },
+  detailInfoSubTitle: {
+    color: "#0767F7",
+    fontSize: 10,
+  },
   reservar: {
     width: "100%",
     paddingHorizontal: 8,
-    marginTop: 10,
+    marginTop: 15,
   },
 });
--- mocks/data.ts
+++ mocks/data.ts
@@ -1,73 +1,93 @@
-export const data = [
+export type Hotel = {
+  id: string;
+  cover: string;
+  name: string;
+  price: number;
+  gallery: Array<{ id: number; photo: string }>;
+};
+
+export const data: Array<Hotel> = [
   {
-    id: 1,
+    id: "1",
     cover: "https://images.pexels.com/photos/2041556/pexels-photo-2041556.jpeg",
     name: "Hotel Legal",
     price: 189,
     gallery: [
       {
         id: 1,
-        photo: "Foto um do Hotel Legal",
+        photo:
+          "https://images.pexels.com/photos/3659683/pexels-photo-3659683.jpeg",
       },
       {
         id: 2,
-        photo: "Foto dois do Hotel Legal",
+        photo:
+          "https://images.pexels.com/photos/2952663/pexels-photo-2952663.jpeg",
       },
       {
         id: 3,
-        photo: "Foto três do Hotel Legal",
+        photo:
+          "https://images.pexels.com/photos/258154/pexels-photo-258154.jpeg",
       },
       {
         id: 4,
-        photo: "Foto quatro do Hotel Legal",
+        photo:
+          "https://images.pexels.com/photos/941861/pexels-photo-941861.jpeg",
       },
     ],
   },
   {
-    id: 2,
+    id: "2",
     cover: "https://images.pexels.com/photos/2041556/pexels-photo-2041556.jpeg",
     name: "Hotel Bacana",
     price: 209,
     gallery: [
       {
         id: 1,
-        photo: "Foto um do Hotel Bacana",
+        photo:
+          "https://images.pexels.com/photos/3659683/pexels-photo-3659683.jpeg",
       },
       {
         id: 2,
-        photo: "Foto dois do Hotel Bacana",
+        photo:
+          "https://images.pexels.com/photos/2952663/pexels-photo-2952663.jpeg",
       },
       {
         id: 3,
-        photo: "Foto três do Hotel Bacana",
+        photo:
+          "https://images.pexels.com/photos/258154/pexels-photo-258154.jpeg",
       },
       {
         id: 4,
-        photo: "Foto quatro do Hotel Bacana",
+        photo:
+          "https://images.pexels.com/photos/941861/pexels-photo-941861.jpeg",
       },
     ],
   },
   {
-    id: 3,
+    id: "3",
     cover: "https://images.pexels.com/photos/2041556/pexels-photo-2041556.jpeg",
     name: "Hotel Chique",
     price: 116,
     gallery: [
       {
         id: 1,
-        photo: "Foto um do Hotel Chique",
+        photo:
+          "https://images.pexels.com/photos/3659683/pexels-photo-3659683.jpeg",
       },
       {
         id: 2,
-        photo: "Foto dois do Hotel Chique",
+        photo:
+          "https://images.pexels.com/photos/2952663/pexels-photo-2952663.jpeg",
       },
       {
         id: 3,
-        photo: "Foto três do Hotel Chique",
+        photo:
+          "https://images.pexels.com/photos/258154/pexels-photo-258154.jpeg",
       },
       {
         id: 4,
-        photo: "Foto quatro do Hotel Chique",
+        photo:
+          "https://images.pexels.com/photos/941861/pexels-photo-941861.jpeg",
       },
     ],
   },
